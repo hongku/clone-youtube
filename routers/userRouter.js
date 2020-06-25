726d875371c648640a6396cd53c1311c563c4df2
hongsun <hongsun3152@gmail.com>
2020-06-25 22:56:46 +0900

    Add controllers

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import routes from '../routes';
+import { users, usersDetail, editProfile, changePassword } from '../controllers/userController';
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req, res) => res.send('users'));
-userRouter.get(routes.userDetail, (req, res) => res.send('users detail'));
-userRouter.get(routes.editProfile, (req, res) => res.send('edit profile'));
-userRouter.get(routes.changePassword, (req, res) => res.send('changePassword'));
+userRouter.get('/', users);
+userRouter.get(routes.userDetail, usersDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
