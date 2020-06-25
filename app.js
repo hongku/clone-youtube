d0790d85796a223f38dad8f8e5a5eb52263e3123
hongsun <hongsun3152@gmail.com>
2020-06-25 21:47:50 +0900

    install babel & middleware

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+const handleHome = (req, res) => {
+  res.send('hello my home');
+};
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan('dev'));
+
+app.get('/', handleHome);
+
+export default app;
