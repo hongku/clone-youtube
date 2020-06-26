8bbc4cfe65b30447c840c0a6d427c8826618f602
hongsun <hongsun3152@gmail.com>
2020-06-26 23:09:56 +0900

    Add header & footer templete

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import routes from '../routes';
-import { users, usersDetail, editProfile, changePassword } from '../controllers/userController';
+import { usersDetail, editProfile, changePassword } from '../controllers/userController';
 
 const userRouter = express.Router();
 
-userRouter.get('/', users);
 userRouter.get(routes.userDetail, usersDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
