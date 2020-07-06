d90ff16fd9c0f33b60ca1cdad662edcd995802b4
hongsun <hongsun3152@gmail.com>
2020-07-06 21:09:50 +0900

    Add users, videos dynamic routing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,24 @@
-export const usersDetail = (req, res) => res.render('usersDetail', { pageTitle: 'User Detail' });
+import routes from '../routes';
+
+export const getJoin = (req, res) => res.render('join', { pageTitle: 'Join' });
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, passwordConfirm }
+  } = req;
+  if (password !== passwordConfirm) {
+    res.status(400).render('join', { pageTitle: 'Join' });
+  } else {
+    // TODO: Register User
+    // TODO: Log User In
+    res.redirect(routes.home);
+  }
+};
+export const getLogin = (req, res) => res.render('login', { pageTitle: 'Login' });
+export const postLogin = (req, res) => {
+  console.log(req.body);
+  res.redirect(routes.home);
+};
+export const logout = (req, res) => res.render('logout', { pageTitle: 'Logout' });
+export const usersDetail = (req, res) => res.render('userDetail', { pageTitle: 'User Detail' });
 export const editProfile = (req, res) => res.render('editProfile', { pageTitle: 'Edit Profile' });
 export const changePassword = (req, res) => res.render('changePassword', { pageTitle: 'Change Password' });
-export const join = (req, res) => res.render('join', { pageTitle: 'Join' });
-export const login = (req, res) => res.render('login', { pageTitle: 'Login' });
-export const logout = (req, res) => res.render('logout', { pageTitle: 'Logout' });
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -6,6 +6,6 @@ const userRouter = express.Router();
 
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
-userRouter.get(routes.userDetail, usersDetail);
+userRouter.get(routes.userDetail(), usersDetail);
 
 export default userRouter;
