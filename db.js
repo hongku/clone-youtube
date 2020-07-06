375ed71cc9f93e8e1e168bd82255bcf84e6d49a4
hongsun <hongsun3152@gmail.com>
2020-07-06 22:42:38 +0900

    install mongoose & connect db

--- db.js
+++ db.js
@@ -1,38 +1,14 @@
-export const videos = [
-  {
-    id: 232321,
-    title: 'my video',
-    description: 'explain text',
-    views: 24,
-    videoFile: 'https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4',
-    creator: {
-      id: 125,
-      name: 'hongsun',
-      email: 'hongsun@my.com'
-    }
-  },
-  {
-    id: 2321,
-    title: 'hello video',
-    description: 'explain text',
-    views: 24,
-    videoFile: 'https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4',
-    creator: {
-      id: 125,
-      name: 'hongsun',
-      email: 'hongsun@my.com'
-    }
-  },
-  {
-    id: 232321,
-    title: 'nice video',
-    description: 'explain text',
-    views: 24,
-    videoFile: 'https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4',
-    creator: {
-      id: 125,
-      name: 'hongsun',
-      email: 'hongsun@my.com'
-    }
-  }
-];
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017/wetube', {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log('✅ Connected to DB');
+const handleError = (error) => console.log(`❗️Error on DB Connection: ${error}`);
+
+db.once('open', handleOpen);
+db.once('error', handleError);
